cae57f0006796b3d44b3dd5341af8caa193ead77
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-05-01 03:57:54 +0900

    feat: api-gateway-service AuthFilter 구현 및 테스트완료

---
+++ api-gateway/src/main/java/org/example/apigateway/jwt/JwtTokenProvider.java
@@ -0,0 +1,22 @@
+package org.example.apigateway.jwt;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider { // jwt 토큰 인증만 사용
+
+    private static final String SECRET = "MySecretKey"; // 설정파일로 빼두기
+
+    public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(SECRET))
+                .build().verify(jwt);
+        return decodedJWT;
+    }
+
+}
